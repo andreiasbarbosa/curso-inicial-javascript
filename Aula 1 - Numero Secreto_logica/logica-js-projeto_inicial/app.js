53a8d7cb40857a2a55e1737b37409b057b880d98
Andreia Barbosa <andreia.caroline3@gmail.com>
2023-11-09 23:01:17 -0300

    game more simple changing numeroMaximo

--- Aula 1 - Numero Secreto_logica/logica-js-projeto_inicial/app.js
+++ Aula 1 - Numero Secreto_logica/logica-js-projeto_inicial/app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo "Número Secreto"');
-let numeroMaximo = 600;
+let numeroMaximo = 500;
 let numeroSecreto = parseInt (Math.random() * numeroMaximo + 1);
 let chute;
 let tentativas = 1;
